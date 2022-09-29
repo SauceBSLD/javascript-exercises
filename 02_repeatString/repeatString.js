2c080dd2046acf8f74ecfc536d2123f0e4f72375
SauceBSLD <saucebosssbmusic@gmail.com>
2022-09-29 18:55:49 +0200

    Second exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let newString = string
+    let i = 1
+    while (i < times) {
+        string += newString;
+        i++;
+    }
+    if (times === 0) {
+        return "";
+    } else if (times < 0) {
+        return "ERROR";
+    }
+    return string;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
