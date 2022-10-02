30cae0cecc8b01315062cbb855c0364b9dbfc902
SauceBSLD <saucebosssbmusic@gmail.com>
2022-10-02 13:31:03 +0200

    Exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    let newSum = 0
+    if (args[0] < 0 || args[1] < 0 ||
+        typeof args[0] != "number" || typeof args[1] != "number") {
+        return "ERROR";
+    } else if (args[0] > args[1]) {
+        for (let i = args[0]; i >= args[1]; i--) {
+            newSum += i;
+        }
+        return newSum;
+    } else {
+        for (let i = args[0]; i <= args[1]; i++) {
+            newSum += i;
+        }
+        return newSum;
+    }
 };
 
 // Do not edit below this line
