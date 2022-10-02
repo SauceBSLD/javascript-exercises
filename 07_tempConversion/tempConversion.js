e26813363cea7aea4b123ebfea0b94bebfc0c69f
SauceBSLD <saucebosssbmusic@gmail.com>
2022-10-02 14:23:43 +0200

    Finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempFar) {
+  newTemp = Math.round(((tempFar - 32) / 1.8) * 10) / 10;
+  return newTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(tempCel) {
+  newTemp = Math.round(((tempCel * 1.8) + 32) * 10) / 10;
+  return newTemp;
 };
 
 // Do not edit below this line
